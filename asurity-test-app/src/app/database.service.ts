7d7332bb0f26a669b425d2ef60cfa1a277679a82
CalebSFullmerBYUI <calebsf@byui.edu>
2022-09-26 17:55:56 -0600

    Added default values to the mock database. Data from database will be sorted.

--- asurity-test-app/src/app/database.service.ts
+++ asurity-test-app/src/app/database.service.ts
@@ -19,12 +19,140 @@ export class DatabaseService {
         zip: "12345"
       },
       contactFrequency: 0
+    },
+    {
+      id: 1,
+      firstName: "Sam",
+      lastName: "Smith",
+      emailAddress: "Sam@outlook.com",
+      phoneNumber: "2222222222",
+      address: {
+        street: "Java Ln",
+        city: "Java Town",
+        state: "NM",
+        zip: "54345"
+      },
+      contactFrequency: 1
+    },
+    {
+      id: 2,
+      firstName: "Jane",
+      lastName: "Doe",
+      emailAddress: "jDoe@yahoo.com",
+      phoneNumber: "8887772222",
+      address: {
+        street: "TownsDale St",
+        city: "Daleworth",
+        state: "NM",
+        zip: "76567"
+      },
+      contactFrequency: 0
+    },
+    {
+      id: 3,
+      firstName: "Scotty",
+      lastName: "Tomson",
+      emailAddress: "ScottTom@gmail.com",
+      phoneNumber: "6360000000",
+      address: {
+        street: "Dale St",
+        city: "South Dale",
+        state: "OK",
+        zip: "11111"
+      },
+      contactFrequency: 2
+    },
+    {
+      id: 4,
+      firstName: "Luis",
+      lastName: "Maywest",
+      emailAddress: "westmay@hotmail.com",
+      phoneNumber: "9999999999",
+      address: {
+        street: "Test Rd",
+        city: "Test Town",
+        state: "CA",
+        zip: "02909"
+      },
+      contactFrequency: 2
+    },
+    {
+      id: 5,
+      firstName: "Bill",
+      lastName: "Wattertown",
+      emailAddress: "wattertown@gmail.com",
+      phoneNumber: "4443333333",
+      address: {
+        street: "1st St",
+        city: "Pendelton",
+        state: "AK",
+        zip: "98678"
+      },
+      contactFrequency: 2
+    },
+    {
+      id: 6,
+      firstName: "Kelly",
+      lastName: "Anderson",
+      emailAddress: "kelland@yahoo.com",
+      phoneNumber: "8882222222",
+      address: {
+        street: "3rd Ln",
+        city: "Johnsville",
+        state: "CT",
+        zip: "62712"
+      },
+      contactFrequency: 0
+    },
+    {
+      id: 7,
+      firstName: "Anna",
+      lastName: "Hughes",
+      emailAddress: "anna@company.org",
+      phoneNumber: "2723333333",
+      address: {
+        street: "House Rd",
+        city: "Yellow Road",
+        state: "MS",
+        zip: "00000"
+      },
+      contactFrequency: 1
+    },
+    {
+      id: 8,
+      firstName: "Dave",
+      lastName: "Smith",
+      emailAddress: "dsmith@yahoo.com",
+      phoneNumber: "44455555555",
+      address: {
+        street: "Lane Ln",
+        city: "City Town",
+        state: "NJ",
+        zip: "33333"
+      },
+      contactFrequency: 0
+    },
+    {
+      id: 9,
+      firstName: "Sue",
+      lastName: "kelly",
+      emailAddress: "suekelly@gmail.com",
+      phoneNumber: "7776666666",
+      address: {
+        street: "Apple St",
+        city: "Appleton",
+        state: "PA",
+        zip: "11111"
+      },
+      contactFrequency: 1
     }
   ];
 
   static newId = 10;
 
   constructor() {
+    //localStorage.setItem("contactsArr", JSON.stringify(DatabaseService.contactsArr));
+    
     if (!localStorage.getItem("contactsArr")) {
       localStorage.setItem("contactsArr", JSON.stringify(DatabaseService.contactsArr));
     }
@@ -38,7 +166,20 @@ export class DatabaseService {
     let contactsArr = localStorage.getItem("contactsArr");
 
     if (contactsArr != null) {
-      return JSON.parse(contactsArr);
+      // Sort by firstName and lastName
+      return JSON.parse(contactsArr).sort((contact1: { firstName: String; lastName: String; }, contact2: { firstName: String; lastName: String; }) => {
+        if (contact1.firstName > contact2.firstName) {
+          return 1;
+        } else if (contact1.firstName < contact2.firstName) {
+          return -1;
+        } else if (contact1.lastName > contact2.lastName) {
+          return 1;
+        } else if (contact1.lastName < contact2.lastName) {
+          return -1;
+        }
+
+        return 0;
+      });
     } else {
       return [];
     }
